1e18135ce1a0245cb8fa610b8890df61068573bc
Daniel Axelsson <daniel.axelsson@accedo.tv>
2021-10-06 15:28:17 +0200

    Final Commit! Done!

--- RomanNumbers2.js
+++ RomanNumbers2.js
@@ -3,10 +3,13 @@ module.exports = {
 
 ArabicToRoman:function(n) {
 
-    if(Number.isInteger(n) != true)
-    {
+    if(Number.isInteger(n) != true) {
+        
         throw new TypeError("n is not an integer")
-
+    }
+    if(n >= 4000)
+    {
+        throw new TypeError("Orginal RomanNumbers was never above 4000")
     }
 
     ArabicNummer = n.toString()
@@ -23,27 +26,141 @@ ArabicToRoman:function(n) {
             case '0': break;
             case '1': 
             if(i === 0 ) {
-                tempresult = result
-                result = tempresult.concat("I"); break;
+                temp = "I"
+                result = temp.concat(result); break;
             }
             else if (i === 1) {
-                result = result.concat("X"); break;
+                temp = "X"
+                result = temp.concat(result); break;
             }
-            case '2': 
-            if(i === 0) {
-                result = result.concat("II"); break;
+            else if (i === 2) {
+                temp = "C"
+                result = temp.concat(result); break;
             }
-            else if(i === 1) {
-                result = result.concat("XX"); break;
+            else if (i === 3) {
+                temp = "M"
+                result = temp.concat(result); break;
             }
 
-            case '3': result = result.concat("III"); break;
-            case '4': result = result.concat("IV"); break;
-            case '5': result = result.concat("V"); break;
-            case '6': result = result.concat("VI"); break;
-            case '7': result = result.concat("VII"); break;
-            case '8': result = result.concat("VIII"); break;
-            case '9': result = result.concat("IX"); break;                       
+            case '2': 
+            if(i === 0) {
+                temp = "II"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "XX"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "CC"
+                result = temp.concat(result); break;
+            }
+            else if (i === 3) {
+                temp = "MM"
+                result = temp.concat(result); break;
+            }
+
+            case '3':
+            if(i === 0) {
+                temp = "III"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "XXX"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "CCC"
+                result = temp.concat(result); break;
+            }
+            else if (i === 3) {
+                temp = "MMM"
+                result = temp.concat(result); break;
+            }
+
+            case '4':
+            if(i === 0) {
+                temp = "IV"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "XL"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "CD"
+                result = temp.concat(result); break;
+            }
+
+            case '5':
+            if(i === 0) {
+                temp = "V"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "L"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "D"
+                result = temp.concat(result); break;
+            }
+
+            case '6':
+            if(i === 0) {
+                temp = "VI"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "LX"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "DC"
+                result = temp.concat(result); break;
+            }
+
+            case '7': 
+            if(i === 0) {
+                temp = "VII"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "LXX"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "DCC"
+                result = temp.concat(result); break;
+            }
+
+            case '8': 
+            if(i === 0) {
+                temp = "VIII"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "LXXX"
+                result = temp.concat(result); break;
+            }
+            else if (i === 2) {
+                temp = "DCCC"
+                result = temp.concat(result); break;
+            }
+
+            case '9':
+            if(i === 0) {
+                temp = "IX"
+                result = temp.concat(result); break;
+            }
+            else if(i === 1) {
+                temp = "XC"
+                result = temp.concat(result); break;
+            } 
+            else if (i === 2) {
+                temp = "CM"
+                result = temp.concat(result); break;
+            }                      
 
         }
 
--- test.js
+++ test.js
@@ -33,5 +33,59 @@ describe('RomanNumbers2', function() {
 
     });
 
+    it("should convert 69 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(69)).to.equal("LXIX")
+
+    });
+
+    it("should convert 80 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(80)).to.equal("LXXX")
+
+    });
+
+    it("should convert 99 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(99)).to.equal("XCIX")
+
+    });
+
+    it("should convert 100 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(100)).to.equal("C")
+
+    });
+
+    it("should convert 649 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(649)).to.equal("DCXLIX")
+
+    });
+
+    it("should convert 1079 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(1079)).to.equal("MLXXIX")
+
+    });
+
+    it("should convert 3999 to roman", function() {
+
+        expect(romannumbers.ArabicToRoman(3999)).to.equal("MMMCMXCIX")
+
+    });
+
+    it("should throw error when over 4000", function() {
+
+        expect(() => romannumbers.ArabicToRoman(4000)).to.throw("Orginal RomanNumbers was never above 4000")
+
+    });
+
+    it("should throw error when 5000", function() {
+
+        expect(() => romannumbers.ArabicToRoman(5000)).to.throw("Orginal RomanNumbers was never above 4000")
+
+    });
+
  
 });
